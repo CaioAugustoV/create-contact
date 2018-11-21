552905723d8e55f5fd72f731ea5faca5c3b19ff9
Caio Augusto <caioaugustov@gmail.com>
2018-11-21 16:19:46 -0200

    Start

---
+++ components/NewContact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class newcontact extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TESTE</h1>
+      </div>
+    )
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NewContact from '../components/NewContact'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NewContact/>
+      </div>
+    );
+  }
+}
+
+export default App;
