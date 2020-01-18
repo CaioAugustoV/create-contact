95df36f3efdb0d38ef4eaf5595999fba8f912274
CaioAugusto <caioaugustov@gmail.com>
2020-01-18 13:54:30 -0300

    Create Arq

---
+++ components/hooks/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from '../common/styled'
+
+export default function index() {
+  return (
+    <div>
+      aaaaaa
+    </div>
+  )
+}
---
+++ pages/hooks.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Page from '../components/hooks'
+
+export default function Hooks() {
+  return <Page />
+}
